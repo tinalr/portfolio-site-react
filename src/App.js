40ebc5470dec177e05fd811c85d62c7fe05d30e9
Christina Rontell <clrontell@gmail.com>
2023-01-16 14:03:04 -0600

    components/Layout setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Layout />}>
       </Route>
     </Routes>
   );
