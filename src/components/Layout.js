b1462447180ee27a5de0ffd89ac85f44232687bc
Christina Rontell <clrontell@gmail.com>
2023-01-19 13:35:58 -0600

    changes to setup tailwind

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
-import Sidebar from '../Sidebar';
-import './index.scss';
+import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/components/*.{html,js}"],
   theme: {
     extend: {},
   },
