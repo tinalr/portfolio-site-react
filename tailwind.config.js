f95731abe6ff8d015705710be2c6f40e29fc8f78
Christina Rontell <clrontell@gmail.com>
2023-01-19 13:53:42 -0600

    starting over with Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/components/*.{html,js}"],
+  content: ["./src/components.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
